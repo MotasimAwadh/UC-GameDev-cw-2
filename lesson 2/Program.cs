7cccc7d44e144d1f4d0b4fabc59c639b445514e9
MotasimAwadh <107761643+MotasimAwadh@users.noreply.github.com>
2022-06-25 18:26:14 +0300

    done (lesson 2)

---
+++ lesson 2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// hero
+string heroName = "Motasim";
+int heroPower = 15;
+
+
+// villair
+string villainName = "university";
+int villainPower = 12;
+
+
+//If Statements
+if (heroPower > villainPower)
+{
+    Console.WriteLine("hero is stronger !");
+}
+
+if (heroPower < villainPower)
+{
+    Console.WriteLine("villair is stronger !");
+}
+
+if(heroPower == villainPower)
+{
+    Console.WriteLine("they are same");
+}
+
+//end
